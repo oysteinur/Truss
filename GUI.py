b4346733b17f741b4c0d6992cb1864b445685f63
oysteinur <oysteinurdal6@gmail.com>
2021-04-18 14:29:46 +0200

    Lagt til GUI.py

---
+++ GUI.py
@@ -0,0 +1,13 @@
+from tkinter import *
+import matplotlib as plt
+from Plot import *
+
+window = Tk()
+
+Heading_Label = Label(window, text="Stavmodell")
+
+Heading_Label.pack()
+
+print
+
+window.mainloop()
